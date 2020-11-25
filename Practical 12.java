34f85aa7948974e70cd316070a108c9842a248e8
shra17n <74871051+shra17n@users.noreply.github.com>
2020-11-25 11:40:09 +0530

    Create Practical 12.java

---
+++ Practical 12.java
@@ -0,0 +1,14 @@
+public class WhileEven {
+
+    public static void main(String[] args) {
+        int i=0;
+        int num=0;
+        System.out.println("Even numbers");
+        while(i<=10)
+        {
+            System.out.println(num);
+            i+=1;
+            num+=2;
+        }
+    }
+}
